48c0b688b428aadb44dde25890a0924c43e523cf
Darshana <keerthi.mac@gmail.com>
2023-03-22 16:56:35 +0530

    mongoDB Database & server config

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+import {
+  getProducts,
+  getProductById,
+} from "../controller/productController.js";
+
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+
+export default router;
